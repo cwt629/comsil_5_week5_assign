bd79363e62a7ef2fecc0f4faa00a40a04b088755
unknown <cwt629@naver.com>
2021-04-03 20:52:17 +0900

    과제 수정2

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 // 20161255 ภๅฟ๘ลย
 int main() {
 	printf("This is original first line\n");
-	printf("This is original second line\n");
+	printf("This is revised second line\n");
 	printf("This is original third line\n");
 	return 0;
 }
